2530cdc541d7b9a94fb7bc0f10e19596ec5fdc87
Saikat-D023 <saikatdey358@gmail.com>
2024-12-11 21:18:38 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className='grid grid-cols-12'>
+      <div className='bg-red-400 col-span-4'>
+        child 1
+      </div>
+      <div className='bg-green-400 col-span-6'>
+        child 2
+      </div>
+      <div className='bg-blue-400 col-span-2'>
+        child 3
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App
