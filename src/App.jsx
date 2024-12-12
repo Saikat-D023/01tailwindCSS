2af0eb7f38ddd0ef95277e1db93f077e0f4f6334
Saikat-D023 <saikatdey358@gmail.com>
2024-12-12 22:46:30 +0530

    working on frontend page

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <>
-    //excercise-1
+    {/* //excercise-1
    <div className='grid grid-cols-12'>
     <div className='col-span-12 sm:col-span-5 bg-red-300 '>
       hi
@@ -15,7 +15,14 @@ function App() {
     <div className='col-span-12 sm:col-span-2 bg-blue-300 '>
       hi
     </div>
-   </div>
+   </div> */}
+
+   {/* excercise-2 */}
+   <div className='h-screen w-full
+   bg-blue-900'>
+    <h1 className='py-300'>Verify Your Age</h1>
+
+    </div>
     </>
   )
 }
