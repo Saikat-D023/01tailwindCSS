26ff2b46684024d0681ed451c262abcb35eb582b
Saikat-D023 <saikatdey358@gmail.com>
2024-12-19 23:26:56 +0530

    init

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -7,4 +7,5 @@ export const Button = ({
     return <span onClick={onClick} className={`rounded-2xl text-2xl px-12 py-2 text-white cursor-pointer ${disabled ? "bg-blue-200" : "bg-green-400"}`}>
         {children}
     </span>
+// sorry
 }
